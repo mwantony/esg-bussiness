d50d620c12c905f2a15b180307d32aef200b6936
Antony Maziero Warken <againplayi7@gmail.com>
2024-06-23 17:22:07 -0300

    sd

---
+++ assets/js/graficoCabecalho.js
@@ -0,0 +1,27 @@
+// Função para desenhar o gráfico do cabeçalho
+function drawCabecalhoChart() {
+  // Dados do gráfico
+  var data = google.visualization.arrayToDataTable([
+    ["Ano", "Vendas", "Despesas"],
+    ["2020", 1000, 400],
+    ["2021", 1170, 460],
+    ["2022", 660, 1120],
+    ["2023", 1030, 540],
+  ]);
+
+  var options = {
+    title: "Lorem Impsum",
+    hAxis: { title: "Year", titleTextStyle: { color: "#333" } },
+    vAxis: { minValue: 0 },
+    fontName: "Poppins",
+    fontSize: 14,
+    height: 400,
+    width: 500
+  };
+
+  // Criação e renderização do gráfico
+  var chart = new google.visualization.AreaChart(
+    document.getElementById("cabecalho__grafico")
+  );
+  chart.draw(data, options);
+}
---
+++ assets/js/loader.js
@@ -0,0 +1,6 @@
+// Carregar a biblioteca de visualização e o pacote de gráficos
+google.charts.load('current', {packages: ['corechart', 'bar']});
+
+// Configurar os callbacks para executar quando a API do Google Charts estiver carregada
+google.charts.setOnLoadCallback(drawCabecalhoChart);
+google.charts.setOnLoadCallback(drawRodapeChart);
